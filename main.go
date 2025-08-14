743afd66a02b2b89fb7169187e2154bc25b79524
Yahya Naji <najiyahya2003@gmail.com>
2025-08-14 20:43:21 +0300

    add /metrics endpoint to display the number of requests processed

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	apiConfig := apiConfig{}
+	mux.Handle("/app/", apiConfig.middlewareMetricsInc(fileServerHandler))
 	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", apiConfig.metricsHandler)
 	server := http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
 }
@@ -14,4 +21,20 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	}
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "Hits: %v", cfg.fileServerHits.Load())
 }
