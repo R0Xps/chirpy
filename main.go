92b709af7c2043b5412f1b94cd279a6b70c25a91
Yahya Naji <najiyahya2003@gmail.com>
2025-08-14 20:50:39 +0300

    refactor healthzHandler function to use the same functions as other handlers

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
-	w.Write([]byte("OK"))
+	fmt.Fprint(w, "OK")
 }
 
 type apiConfig struct {
