397c5fe7a804595374add6d603b86b403d8a06cf
Yahya Naji <najiyahya2003@gmail.com>
2025-08-14 18:15:40 +0300

    add 'healthz' readiness endpoint

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthzHandler)
 	server := http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
