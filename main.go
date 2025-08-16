9d59c699215d4a2925c0dec16dba821b82fe4e18
Yahya Naji <najiyahya2003@gmail.com>
2025-08-16 12:36:05 +0300

    add validate_chirp endpoint

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
@@ -14,6 +16,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", healthzHandler)
 	mux.HandleFunc("GET /admin/metrics", apiConfig.metricsHandler)
 	mux.HandleFunc("POST /admin/reset", apiConfig.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 	server := http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
 }
@@ -50,4 +53,51 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.fileServerHits.Store(0)
 	w.WriteHeader(200)
 	fmt.Fprint(w, "OK")
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	type responseVals struct {
+		Valid bool   `json:"valid"`
+		Error string `json:"error"`
+	}
+
+	resBody := responseVals{}
+	if len(params.Body) > 140 {
+		resBody.Valid = false
+		resBody.Error = "Chirp is too long"
+	} else {
+		resBody.Valid = true
+		resBody.Error = ""
+	}
+
+	data, err := json.Marshal(resBody)
+
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if resBody.Valid {
+		w.WriteHeader(200)
+	} else {
+		w.WriteHeader(400)
+	}
+
+	w.Write(data)
 }
