c6ceb47b352b7e61348c32cb8484596db399048b
Yahya Naji <najiyahya2003@gmail.com>
2025-08-16 11:53:22 +0300

    update metrics endpoint to return html instead of plain text for better readability

--- main.go
+++ main.go
@@ -36,8 +36,14 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(200)
-	fmt.Fprintf(w, "Hits: %v", cfg.fileServerHits.Load())
+	fmt.Fprintf(w, `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileServerHits.Load())
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
