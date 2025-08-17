f646186f07fcb17bafbdc3db87bb0561e815e427
Yahya Naji <najiyahya2003@gmail.com>
2025-08-17 22:56:58 +0300

    add GetChirpsInOrder query

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -4,3 +4,8 @@ VALUES (
     gen_random_uuid(), NOW(), NOW(), $1, $2
 )
 RETURNING *;
+
+-- name: GetChirpsInOrder :many
+SELECT id, created_at, updated_at, body, user_id
+FROM chirps
+ORDER BY created_at ASC;
